4621687e65fe81444885336d7b8f408eb5c72dd3
BoMarconiHenriksen <bomarconi@gmail.com>
2019-10-17 12:53:54 +0200

    Example of IEnumerable extension

---
+++ IEnumerableExtensions/ConsoleApp1/AssetListViewAsset.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+  public class AssetListViewAsset
+  {
+    public int AssetID { get; set; }
+
+    public int AssetTypeDefinitionID { get; set; }
+
+    public bool Deleted { get; set; }
+
+    public IEnumerable<StringValue> StringValues { get; set; }
+
+    public IEnumerable<NumberValue> NumberValues { get; set; }
+
+    public IEnumerable<DateTimeValue> DateTimeValues { get; set; }
+
+    public IEnumerable<BlobValue> BlobImageValues { get; set; }
+
+    public IEnumerable<AssetListViewAsset> FromRelations { get; set; }
+
+    public class StringValue
+    {
+      public int AssetFieldDefinitionID { get; set; }
+
+      public string Value { get; set; }
+    }
+
+    public class NumberValue
+    {
+      public int AssetFieldDefinitionID { get; set; }
+
+      public decimal? Value { get; set; }
+    }
+
+    public class DateTimeValue
+    {
+      public int AssetFieldDefinitionID { get; set; }
+
+      public DateTime? Value { get; set; }
+    }
+
+    public class BlobValue
+    {
+      public int AssetFieldDefinitionID { get; set; }
+
+      public byte[] Value { get; set; }
+    }
+  }
+}
---
+++ IEnumerableExtensions/ConsoleApp1/IEnumerableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+  public static class IEnumerableExtensions
+  {
+    public static IEnumerable<IEnumerable<T>> ComputePaths<T>(T Root, Func<T, IEnumerable<T>> Children)
+    {
+      var children = Children(Root); 
+      if (children != null && children.Any())
+      {
+        foreach (var Child in children)
+          foreach (var ChildPath in ComputePaths(Child, Children))
+            yield return new[] { Root }.Concat(ChildPath);
+      }
+      else
+      {
+        yield return new[] { Root };
+      }
+    }
+
+    public static IEnumerable<IEnumerable<T>> ComputePathsForAllItems<T>(this IEnumerable<T> assetList, Func<T, IEnumerable<T>> funcChildren)
+    {
+      return assetList.SelectMany(a => IEnumerableExtensions.ComputePaths(a, funcChildren));
+    }
+  }
+}
