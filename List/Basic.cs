d757e12db54252f396da08df3a1d7f44e655631a
BoMarconiHenriksen <bomarconi@gmail.com>
2019-10-21 11:31:52 +0200

    Added the method to call the extension

---
+++ List/Basic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+  class Basic
+  {
+    public void Hello()
+    {
+      Console.WriteLine("Hello World!");
+
+    }
+  }
+}
---
+++ List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var basic = new Basic();
+      basic.Hello();
+
+
+      var presidents = new List<string>
+      {
+        "Jimmy Carter",
+        "Ronald Reagan",
+        "George HW Bush",
+        "George W Bush"
+      };
+
+      presidents.Add("Barak Obama");
+
+      foreach (string president in presidents)
+        Console.WriteLine(president);
+
+    }
+  }
+}
